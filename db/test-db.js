6172dd4e4823675e084f5f764ad827017640eaec
Eber <www.eberacunaquispe@gmail.com>
2026-08-28 19:07:57 -0500

    Pruebas de conexion a postgre

--- db/test-db.js
+++ db/test-db.js
@@ -2,6 +2,9 @@ import pool from './pool.js';
 
 try {
     const resultado = await pool.query('SELECT NOW()');
+    const { rows } = await pool.query('SELECT * FROM products;');
+
+    console.log(rows);
 
     console.log('Conexión exitosa');
     console.log(resultado.rows);
