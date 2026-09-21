a626fa22dfb53a307b4a829c665079572594b179
Eber <www.eberacunaquispe@gmail.com>
2026-09-21 17:27:04 -0500

    Se convierte el id a numero antes de validar para asegurarse de que no sea interpretado como string

--- middleware/validateId.js
+++ middleware/validateId.js
@@ -1,7 +1,8 @@
 export function validateId(req, res, next) {
     const { id } = req.params;
+    const idNumber = Number(id);
 
-    if (!Number.isInteger(id) || id <= 0) {
+    if (!Number.isInteger(idNumber) || idNumber <= 0) {
         return res.status(400).json({
             error: 'El ID debe ser un entero positivo',
         });
