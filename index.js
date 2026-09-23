9b2159441e7ddad83a0239b57ec26974aeebd8e2
Eber <www.eberacunaquispe@gmail.com>
2026-09-23 17:36:11 -0500

    Se configura la ruta raiz asimismo se añadio el methodOverriede que permite hacer put y delete y las rutas absolutas

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import rutascategoria from './routes/categoryRouter.js';
 import rutasproducto from './routes/productsRouter.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import methodOverride from 'method-override';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/categories', rutascategoria);
 app.use('/products', rutasproducto);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(3000, () => {
     console.log('Servidor ejecutándose en http://localhost:3000');
